2c8869be3fb2b085499a6c0397c96c8ba6b3662f
Zoom <dnnyfst@outlook.com>
2026-04-10 14:44:12 +0200

    Remove font size control elements
    - already browser integrated

--- js/common.js
+++ js/common.js
@@ -51,22 +51,6 @@ const Common = (() => {
         controls.insertBefore(picker, controls.firstChild);
     }
 
-    function initFontSize() {
-        let fontSize = parseInt(localStorage.getItem('stnh_wiki_fontsize')) || 118;
-        document.documentElement.style.setProperty('--base-font-size', fontSize + '%');
-
-        function adjust(delta) {
-            fontSize = Math.min(160, Math.max(90, fontSize + delta));
-            document.documentElement.style.setProperty('--base-font-size', fontSize + '%');
-            localStorage.setItem('stnh_wiki_fontsize', fontSize);
-        }
-
-        const btnDown = document.getElementById('font-size-down');
-        const btnUp = document.getElementById('font-size-up');
-        if (btnDown) btnDown.addEventListener('click', () => adjust(-10));
-        if (btnUp) btnUp.addEventListener('click', () => adjust(10));
-    }
-
     function initLangSelect() {
         const sel = document.getElementById('lang-select');
         if (!sel) return;
@@ -443,7 +427,6 @@ const Common = (() => {
     function init() {
         initTheme();
         injectThemePicker();
-        initFontSize();
         initLangSelect();
         initNavHighlight();
         initHamburger();
